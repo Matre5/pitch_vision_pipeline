cbceb6f66dfd09dc880a90e54e849088d0d571ea
Matre Aiguokhian <aigmatre5@gmail.com>
2026-08-10 16:17:16 +0100

    Csv tracked ids

--- src/track.py
+++ src/track.py
@@ -12,11 +12,19 @@ results = model.track(
     project="outputs",
     name="tracking_test_BSrt_Conf",
     verbose=False,
-    device=0,
+    # device=0,
     stream=True
 )
 
-header = 
+header = ['frame', 'track_id', 'x1', 'y1', 'x2', 'y2']
 
-with open("player_info.csv", "w", newline=" ", encoding="utf-8") as file:
+with open("track_info.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
+    
+    writer.writerow(header)
+    
+    for frame_num, result in enumerate(results):
+        if result.boxes.id is not None:
+            for track_id, box in zip(result.boxes.id, result.boxes.xyxy):
+                x1, y1, x2, y2 = map(int, box)
+                writer.writerow([frame_num, int(track_id), x1, y1, x2, y2])
