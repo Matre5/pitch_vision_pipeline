52732e5aebd98c4953c4b60ca02f52e203a711c6
Matre Aiguokhian <aigmatre5@gmail.com>
2026-08-11 09:17:05 +0100

    team_id building

--- team_id.py
+++ team_id.py
@@ -1,15 +1,31 @@
 import cv2
+import csv
 import numpy as np
 from sklearn.cluster import KMeans
-from src import track
 
-# def get_torso(frame, box):
-#     pass
+def get_torso(frame, x1, y1, x2, y2):
+    height = y2 -y1
+    top_torso = y1 + int(height * 0.20)
+    bottom_torso = y1 + int(height * 0.55)
+    
+    return frame[top_torso:bottom_torso, x1:x2]
 
-# print(type(box))
+def get_dominant_hue(torso_crop):
+    if torso_crop.size == 0:
+        return None
+    hsv = cv2.cvtColor(torso_crop, cv2.COLOR_BGR2HSV)
+    return np.mean(hsv[:, :, 0])
 
-first_fr = track.results[0]
-boxess = first_fr.boxes.xyxy
+cap = cv2.VideoCapture("data/worldcup_clip_trimmed.mp4")
 
-print(type(boxess))
-print(boxess)
+hue_data = []
+
+with open("data/track_info.csv", "r") as file:
+    reader = csv.DictReader(file)
+    rows = list(reader)
+    
+    
+    
+    
+    
+
