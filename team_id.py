aad6db5550adae5154840f114b72f978d49106ba
Matre Aiguokhian <aigmatre5@gmail.com>
2026-08-12 20:01:01 +0100

    team_id building

--- team_id.py
+++ team_id.py
@@ -20,10 +20,28 @@ cap = cv2.VideoCapture("data/worldcup_clip_trimmed.mp4")
 
 hue_data = []
 
+current_frame_num = -1
+current_frame_img = None
+
 with open("data/track_info.csv", "r") as file:
     reader = csv.DictReader(file)
-    rows = list(reader)
-    
+    # rows = list(reader)
+    for row in reader:
+        frame_num = int(row['frame'])
+        
+        if frame_num != current_frame_num:
+            ret, frame = cap.read()
+            
+            if not ret:
+                print("Failed to read frame, sorry....")
+                break
+            
+            current_frame_img = frame
+            current_frame_num = frame_num
+            
+        x1, y1, x2, y2 = int(row['x1']), int(row['y1']), int(row['x2']), int(row['y2'])
+
+            
     
     
     
