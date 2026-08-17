259ad2a87cfdc2100995762be6cd8877ae74999e
Matre Aiguokhian <aigmatre5@gmail.com>
2026-08-17 13:05:57 +0100

    clustering

--- team_id.py
+++ team_id.py
@@ -2,9 +2,11 @@ import cv2
 import csv
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from collections import defaultdict
 
 def get_torso(frame, x1, y1, x2, y2):
-    height = y2 -y1
+    height = y2 - y1
     top_torso = y1 + int(height * 0.20)
     bottom_torso = y1 + int(height * 0.55)
     
@@ -14,7 +16,9 @@ def get_dominant_hue(torso_crop):
     if torso_crop.size == 0:
         return None
     hsv = cv2.cvtColor(torso_crop, cv2.COLOR_BGR2HSV)
-    return np.mean(hsv[:, :, 0])
+    hue = np.mean(hsv[:, : , 0])
+    sat = np.mean(hsv[:, : , 1])
+    return hue, sat
 
 cap = cv2.VideoCapture("data/worldcup_clip_final.mp4")
 
@@ -44,12 +48,26 @@ with open("data/track_info.csv", "r") as file:
         torso = get_torso(current_frame_img, x1, y1, x2, y2)
         hue = get_dominant_hue(torso)
         
-        if hue is not None:
-            hue_data.append((int(row['track_id']), hue))
+        result = get_dominant_hue(torso)
+        if result is not None:
+            hue, sat = result
+            hue_data.append((int(row['track_id']), hue, sat))
+ 
+    
+# print(f"Total hue readings: {len(hue_data)}")
+# print(hue_data[:15])
 
-            
     
     
-    
-    
+# Clustering
+track_ids = [item[0] for item in hue_data]
+features = [[item[1], item[2]] for item in hue_data] # getting the hue and sat
 
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
+
+kmeans = KMeans(n_clusters=3, random_state=42)
+labels = kmeans.fit_predict(scaled_features)
+
+for i in range(15):
+    print(f"track_id: {track_ids[i]}, hue: {features[i][0]:.1f}, sat: {features[i][1]:.1f}, cluster: {labels[i]}")
